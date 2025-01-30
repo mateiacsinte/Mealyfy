503b563b2dd1ffa0ebba2aa3a3c512a3f2cc4846
mateiacsinte <acsinte.matei.ionut@gmail.com>
2025-01-30 15:41:10 +0200

    Initial commit

---
+++ src/main/java/com/mealyfy/MealyfyApplication.java
@@ -0,0 +1,13 @@
+package com.mealyfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealyfyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealyfyApplication.class, args);
+	}
+
+}
